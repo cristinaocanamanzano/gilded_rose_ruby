bbfe0248b3955875bf5803f1d2700d0b49f9919c
Cristina María Ocaña Manzano <cristinamariaocanamanzano@MacBook-Air-de-Cristina.local>
2019-01-03 19:15:15 +0100

    Delegated update of sell_in date to private function

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,6 +6,9 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+
+      update_regular_item_sellin(item)
+      
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
@@ -29,9 +32,7 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -49,6 +50,14 @@ class GildedRose
           end
         end
       end
+    end
+  end
+
+  private
+
+  def update_regular_item_sellin(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
     end
   end
 end
