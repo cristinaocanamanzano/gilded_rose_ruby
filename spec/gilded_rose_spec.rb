bb2f426df24a2130ddfecdf612c8d615ea3b659e
Cristina María Ocaña Manzano <cristinamariaocanamanzano@MacBook-Air-de-Cristina.local>
2019-01-03 18:56:39 +0100

    Test for regular quality and sell in date behaviour

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,26 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  regular_item_1 = Item.new("item_1", 3, 5)
+  regular_item_2 = Item.new("item_2", 1, 3)
+
+  items = [regular_item_1, regular_item_2]
+
+  subject(:gilded_rose) { described_class.new(items)}
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    context "regular items" do
+      context "when update_quality has been run once" do
+        it "reduces sell in date by 1" do
+          gilded_rose.update_quality
+          expect(items[0].sell_in).to eq 2
+          expect(items[1].sell_in).to eq 0
+        end
+
+        it "reduces quality by 1 when sell in date is positive" do
+          expect(items[0].quality).to eq 4
+        end
+      end
     end
   end
 
