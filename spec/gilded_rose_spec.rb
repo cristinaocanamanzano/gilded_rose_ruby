1381d1c286d3dd33c51f7eb2226ade1a3f35d14d
Cristina María Ocaña Manzano <cristinamariaocanamanzano@MacBook-Air-de-Cristina.local>
2019-01-03 19:03:34 +0100

    Tests for sulfura item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,8 +3,11 @@ require 'gilded_rose'
 describe GildedRose do
   regular_item_1 = Item.new("item_1", 3, 5)
   regular_item_2 = Item.new("item_2", 1, 3)
+  sulfura = Item.new("Sulfuras, Hand of Ragnaros", 10, 10)
+  brie = Item.new("Aged Brie", 10, 15)
+  backstage= Item.new("Backstage passes to a TAFKAL80ETC concert", 25, 5)
 
-  items = [regular_item_1, regular_item_2]
+  items = [regular_item_1, regular_item_2, sulfura, brie, backstage]
 
   subject(:gilded_rose) { described_class.new(items)}
 
@@ -32,7 +35,26 @@ describe GildedRose do
           expect(items[1].sell_in).to eq -1
         end
       end
+
+      context "when update_quality has been run 12 times" do
+       it "quality is never negative" do
+         10.times { gilded_rose.update_quality }
+         expect(items[0].quality).to eq 0
+         expect(items[1].quality).to eq 0
+       end
+      end
+    end
+
+    context "special items" do
+       context "when update_quality has been run 12 times" do
+         it "sulfura item does not have its quality affected" do
+           expect(items[2].sell_in).to eq 10
+         end
+
+         it "sulfura item does not have its sellin date affected" do
+           expect(items[2].quality).to eq 10
+         end
+       end
     end
   end
-
 end
